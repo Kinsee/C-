76317691d64f65ed33294b7be72c0480dbe1b3f1
Mar'ya <kinsv@mail.ru>
2022-06-21 20:07:22 +0300

    add Program

---
+++ Example001_HelloConsole/Program.cs
@@ -0,0 +1,5 @@
+﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Enter your name ");
+string username =  Console.ReadLine();
+Console.Write(("Hello,"));
+Console.Write(username);Console.WriteLine("2 Hello, World! ");
